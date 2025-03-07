c8f87bfae8a8ded0c73851784c423569957b20ae
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-07 15:59:34 +0000

    add controller to handle routing

---
+++ src/ServerVariables/Controllers/ServerVariablesController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Profiling.Internal;
+
+namespace ServerVariables.Controllers;
+
+[Route("/App_Plugins/ServerVariables/")]
+public class ServerVariablesController(IConfiguration configuration) : ControllerBase
+{
+    [HttpGet("AppSettings.js")]
+    public IActionResult AppSettings()
+    {
+        // Check for the presence of the configuration section
+        IConfigurationSection serverVariablesSection = configuration.GetSection("ServerVariables");
+        Dictionary<string, string?> serverVariables = serverVariablesSection.AsEnumerable().Where(x => !string.IsNullOrEmpty(x.Value)).ToDictionary(x => x.Key.Replace("ServerVariables:", string.Empty), x => x.Value);
+
+        Response.Headers.Add("Content-Type", "application/javascript");
+        return Content($"export default {serverVariables.ToJson()}");
+    }
+
+    [HttpGet("{filename}.js")]
+    public IActionResult Index(string filename)
+    {
+        // return random content and header for javascript
+        Response.Headers.Add("Content-Type", "application/javascript");
+        return Content("export default { 'apiKey': '123456789', 'apiUrl': 'https://api.example.com', 'fromServer': '" + filename + "' };");
+    }
+}
