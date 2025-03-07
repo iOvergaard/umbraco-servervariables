bba1102d34639abb65219e73771657bfa0bdb25d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-07 15:59:24 +0000

    add package project

---
+++ src/ServerVariables/Composers/ServerVariablesComposer.cs
@@ -0,0 +1,11 @@
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace ServerVariables.Composers
+{
+    public class ServerVariablesApiComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        { }
+    }
+}
