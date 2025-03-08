ca67c5fa2aa3bfe910850b19b730c86c8cca4d5b
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-08 14:01:42 +0000

    update docs

--- src/ServerVariables/Options.cs
+++ src/ServerVariables/Options.cs
@@ -7,21 +7,21 @@ namespace ServerVariables;
 public class ServerVariablesOptions
 {
     /// <summary>
-    ///     Gets or sets a value indicating whether to allow public access to the server variables.
+    ///     Gets a value indicating whether to allow public access to the server variables.
     /// </summary>
     /// <remarks>This will expose the variables on the login screen.</remarks>
     [DefaultValue(false)]
     public bool AllowPublicAccess { get; set; } = false;
 
     /// <summary>
-    ///     Gets or sets the namespace for the server variables.
+    ///     Gets the namespace for the server variables.
     /// </summary>
     /// <remarks>This defaults to "vars" and is the path where you import the variables from.</remarks>
     [DefaultValue("vars")]
     public string Namespace { get; set; } = "vars";
 
     /// <summary>
-    ///     Gets or sets the cache header value for the server variables.
+    ///     Gets the cache header value for the server variables.
     /// </summary>
     /// <example></example>
     [DefaultValue("no-cache, no-store, must-revalidate")]
