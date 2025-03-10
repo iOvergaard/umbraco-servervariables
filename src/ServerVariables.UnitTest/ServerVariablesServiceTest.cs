2b4c210a2b58bfd57190149bd889c5a25f45edda
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-10 15:16:56 +0100

    add test project

---
+++ src/ServerVariables.UnitTest/ServerVariablesServiceTest.cs
@@ -0,0 +1,74 @@
+using Microsoft.Extensions.Configuration;
+using ServerVariables.Services;
+
+namespace ServerVariables.UnitTest;
+
+public class ServerVariablesServiceTest
+{
+    private IServerVariablesService _serverVariablesService;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Arrange
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("test_appsettings.json")
+            .Build();
+        _serverVariablesService = new ServerVariablesService(configuration);
+    }
+
+    [Test]
+    public void Test_GetAppSettings()
+    {
+
+        // Act
+        Dictionary<string, string?> appSettings = _serverVariablesService.GetAppSettings();
+
+        // Assert
+        Assert.That(appSettings, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(appSettings, Is.Not.Empty);
+            Assert.That(appSettings.ContainsKey("FromAppSettings"), Is.True);
+            Assert.That(appSettings["FromAppSettings"], Is.EqualTo("Hello from test_appsettings.json"));
+        });
+    }
+
+    [Test]
+    public void Test_SetVariable()
+    {
+        // Arrange
+        _serverVariablesService.SetVariable("FromTest", "Hello from test");
+
+        // Act
+        Dictionary<string, string?> section = _serverVariablesService.GetSection("index");
+
+        // Assert
+        Assert.That(section, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(section, Is.Not.Empty);
+            Assert.That(section.ContainsKey("FromTest"), Is.True);
+            Assert.That(section["FromTest"], Is.EqualTo("Hello from test"));
+        });
+    }
+
+    [Test]
+    public void Test_SetVariableWithSection()
+    {
+        // Arrange
+        _serverVariablesService.SetVariable("FromTest", "Hello from test", "test");
+
+        // Act
+        Dictionary<string, string?> section = _serverVariablesService.GetSection("test");
+
+        // Assert
+        Assert.That(section, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(section, Is.Not.Empty);
+            Assert.That(section.ContainsKey("FromTest"), Is.True);
+            Assert.That(section["FromTest"], Is.EqualTo("Hello from test"));
+        });
+    }
+}
