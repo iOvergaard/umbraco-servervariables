f611e638a056c291bc9787aec6ccc27a9129214f
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-13 09:12:03 +0100

    feat: adds ordering and filtering

--- src/ServerVariables/ApiControllers/ServerVariablesItemsApiController.cs
+++ src/ServerVariables/ApiControllers/ServerVariablesItemsApiController.cs
@@ -17,7 +17,6 @@ public class ServerVariablesItemsApiController(IServerVariablesService serverVar
     public IActionResult Items(
         CancellationToken cancellationToken,
         string orderBy = "section",
-        string? orderCulture = null,
         Direction orderDirection = Direction.Ascending,
         string? filter = null,
         int skip = 0,
@@ -46,12 +45,38 @@ public class ServerVariablesItemsApiController(IServerVariablesService serverVar
             }
         }
 
+        var totalNumberOfItems = items.Count;
+
+        // Use filter to filter by a custom field
+        if (!string.IsNullOrWhiteSpace(filter))
+        {
+            items = items
+                .Where(x => x.Key.Contains(filter, StringComparison.OrdinalIgnoreCase) ||
+                            x.Value.ToString().Contains(filter, StringComparison.OrdinalIgnoreCase) ||
+                            x.Section.Contains(filter, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+        }
+
+        // Use orderBy to order by a custom field
+        IOrderedEnumerable<ServerVariablesCollectionResponseModel> orderedList = orderBy switch
+        {
+            "key" when orderDirection == Direction.Ascending => items.OrderBy(x => x.Key),
+            "key" when orderDirection == Direction.Descending => items.OrderByDescending(x => x.Key),
+            "value" when orderDirection == Direction.Ascending => items.OrderBy(x => x.Value),
+            "value" when orderDirection == Direction.Descending => items.OrderByDescending(x => x.Value),
+            "section" when orderDirection == Direction.Ascending => items.OrderBy(x => x.Section),
+            "section" when orderDirection == Direction.Descending => items.OrderByDescending(x => x.Section),
+            _ => throw new ArgumentOutOfRangeException(nameof(orderBy), orderBy, "Invalid order by field")
+        };
+
+        items = orderedList.ToList();
+
         items = items
             .Skip(skip)
             .Take(take)
             .ToList();
 
-        return CollectionResult(items, items.Count);
+        return CollectionResult(items, totalNumberOfItems);
     }
 
     private OkObjectResult CollectionResult(List<ServerVariablesCollectionResponseModel> collectionResponseModels, long totalNumberOfItems)
