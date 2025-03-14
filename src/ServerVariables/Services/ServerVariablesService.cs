9174e4223bd276385bd5a29fd7942432c1c77582
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-14 20:57:12 +0100

    move logic to service and implement Attempt pattern

--- src/ServerVariables/Services/ServerVariablesService.cs
+++ src/ServerVariables/Services/ServerVariablesService.cs
@@ -1,4 +1,7 @@
 using Microsoft.Extensions.Options;
+using ServerVariables.Models;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Services;
 
 namespace ServerVariables.Services;
 
@@ -69,5 +72,70 @@ public class ServerVariablesService(IOptions<ServerVariablesOptions> options) : 
     public Dictionary<string, Dictionary<string, dynamic>> GetAll()
     {
         return _serverVariables;
+    }
+
+    public Attempt<IEnumerable<ServerVariablesCollectionResponseModel>?, CollectionOperationStatus> GetPagedItems(string orderBy, Direction orderDirection, string? filter, int skip, int take, CancellationToken cancellationToken, out int totalNumberOfItems)
+    {
+        totalNumberOfItems = 0;
+
+        if (take < 0 || skip < 0 || skip % take != 0)
+        {
+            return Attempt.FailWithStatus<IEnumerable<ServerVariablesCollectionResponseModel>?, CollectionOperationStatus>(CollectionOperationStatus.InvalidSkipTake, null);
+        }
+
+        List<ServerVariablesCollectionResponseModel> items = [];
+        foreach (var (section, variables) in GetAll())
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Attempt.SucceedWithStatus<IEnumerable<ServerVariablesCollectionResponseModel>?, CollectionOperationStatus>(CollectionOperationStatus.Success, null);
+            }
+            foreach (var (key, value) in variables)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return Attempt.SucceedWithStatus<IEnumerable<ServerVariablesCollectionResponseModel>?, CollectionOperationStatus>(CollectionOperationStatus.Success, null);
+                }
+                items.Add(new ServerVariablesCollectionResponseModel
+                {
+                    Key = key,
+                    Value = value,
+                    Section = section,
+                });
+            }
+        }
+
+        totalNumberOfItems = items.Count;
+
+        // Use filter to filter by a custom field
+        if (!string.IsNullOrWhiteSpace(filter))
+        {
+            items = items
+                .Where(x => x.Key.Contains(filter, StringComparison.OrdinalIgnoreCase) ||
+                            x.Value.ToString().Contains(filter, StringComparison.OrdinalIgnoreCase) ||
+                            x.Section.Contains(filter, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+        }
+
+        // Use orderBy to order by a custom field
+        IOrderedEnumerable<ServerVariablesCollectionResponseModel> orderedList = orderBy switch
+        {
+            "key" when orderDirection == Direction.Ascending => items.OrderBy(x => x.Key),
+            "key" when orderDirection == Direction.Descending => items.OrderByDescending(x => x.Key),
+            "value" when orderDirection == Direction.Ascending => items.OrderBy(x => x.Value),
+            "value" when orderDirection == Direction.Descending => items.OrderByDescending(x => x.Value),
+            "section" when orderDirection == Direction.Ascending => items.OrderBy(x => x.Section),
+            "section" when orderDirection == Direction.Descending => items.OrderByDescending(x => x.Section),
+            _ => throw new ArgumentOutOfRangeException(nameof(orderDirection), orderDirection, null)
+        };
+
+        items = orderedList.ToList();
+
+        items = items
+            .Skip(skip)
+            .Take(take)
+            .ToList();
+
+        return Attempt.SucceedWithStatus<IEnumerable<ServerVariablesCollectionResponseModel>?, CollectionOperationStatus>(CollectionOperationStatus.Success, items);
     }
 }
