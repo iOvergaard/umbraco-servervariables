222740bb7942cfba8f977a79c06dfbb1f6e9c471
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-15 21:45:39 +0100

    remove todo

--- src/ServerVariables/wwwroot/client/repositories/sv-repository.js
+++ src/ServerVariables/wwwroot/client/repositories/sv-repository.js
@@ -3,8 +3,6 @@ import { makeRequest } from "../functions.js";
 
 export class SvRepositoryVariables extends UmbRepositoryBase {
   async requestCollection(filter) {
-    debugger;
-
     try {
       const urlParams = new URLSearchParams(filter);
       const data = await makeRequest(this._host, `/umbraco/servervariables/api/v1/items?${urlParams}`, 'GET');
